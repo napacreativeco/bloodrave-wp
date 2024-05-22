6036d5c7a3ab6fee4dbc8df602fde02b98d61306
throne <contact@throne.work>
2024-05-22 16:06:25 -0700

    device motion test

--- compiled/throne.js
+++ compiled/throne.js
@@ -35,7 +35,7 @@ function handleOrientation(event) {
 
     reader.innerHTML = x + ", " + y + ", " + z;
 
-    modelViewer.cameraOrbit = `${x}deg` + `${z}deg ` + `"110%"`;
+    modelViewer.cameraOrbit = `${x * 100}deg` + `${z * 100}deg` + `"110%"`;
   
 // Do something awesome.
 }
