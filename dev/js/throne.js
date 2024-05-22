1fc6af5ea25465be8da4db122898d8108bd3ad78
throne <contact@throne.work>
2024-05-22 16:54:20 -0700

    device motion test

--- dev/js/throne.js
+++ dev/js/throne.js
@@ -28,7 +28,7 @@ document.onmousemove = function(e) {
 }
 
 function handleOrientation(event) {
-    const x = event.accelerationIncludingGravity.x * 20;
+    const x = event.accelerationIncludingGravity.x * 10;
     const y = event.accelerationIncludingGravity.y;
     const z = event.accelerationIncludingGravity.z * 10;
     
