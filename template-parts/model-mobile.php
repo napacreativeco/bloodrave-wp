dccb81de73f3d32c03970d4c435e4ceb530839b3
throne <contact@throne.work>
2024-05-22 16:37:24 -0700

    device motion test

--- template-parts/model-mobile.php
+++ template-parts/model-mobile.php
@@ -1,3 +1,3 @@
 <div class="mobile-model" id="model-holder-mobile">
-    <model-viewer id="myModelMobile" alt="" src="<?php echo get_template_directory_uri(); ?>/assets/bm-logo.glb" disable-zoom disable-tap camera-orbit="15deg -15deg 100m" shadow-intensity="0" touch-action="pan-y" tone-mapping="neutral"></model-viewer>
+    <model-viewer id="myModelMobile" alt="" src="<?php echo get_template_directory_uri(); ?>/assets/bm-logo.glb" disable-zoom disable-tap camera-orbit="2deg 0deg 100m" shadow-intensity="0" touch-action="pan-y" tone-mapping="neutral"></model-viewer>
 </div>
