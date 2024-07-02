9bd1d44083d44a1e358907ce0738a0eb9d465051
throne <contact@throne.work>
2024-07-01 17:29:18 -0700

    new show added

--- template-parts/show-list.php
+++ template-parts/show-list.php
@@ -1,6 +1,17 @@
 <ul class="shows-list">
     <li>
         <div class="info">
+            <p class="date">08.16.24</p>
+            <p class="lineup model-opener" data-image="http://bloodrave.tech/wp-content/uploads/2024/07/ex-heir-portland-square.png">Ex-Heir, Static Ghost, Blood Rave</p>
+        </div> 
+        <div class="flier">
+            <p class="flier-link model-opener" title="Ex-Heir, Static Ghost, Blood Rave. Live at The Coffin Club in Portland, Oregon." data-image="http://bloodrave.tech/wp-content/uploads/2024/07/ex-heir-portland-square.png">
+                View Flier
+            </p>
+        </div>
+    </li>
+    <li>
+        <div class="info">
             <p class="date">05.31.24</p>
             <p class="lineup model-opener" data-image="http://bloodrave.tech/wp-content/uploads/2024/05/show-flier.jpeg">Houses of Heaven, Ms. Boan, Blood Rave</p>
         </div> 
