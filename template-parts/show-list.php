7cb41252e96ccd0e560536b954d0ee9c11f41b8c
throne <contact@throne.work>
2024-07-14 13:39:01 -0700

    show additions

--- template-parts/show-list.php
+++ template-parts/show-list.php
@@ -1,11 +1,47 @@
 <ul class="shows-list">
     <li>
         <div class="info">
-            <p class="date">08.16.24</p>
-            <p class="lineup modal-opener" data-image="http://bloodrave.tech/wp-content/uploads/2024/07/ex-heir-portland-square.png">Ex-Heir, Static Ghost, Blood Rave</p>
+            <p class="lineup modal-opener" data-image="https://bloodrave.tech/wp-content/uploads/2024/07/thee-stork-club-tassel-still-ruins-blood-rave.jpg">Tassel, Still Ruins, Blood Rave</p>
         </div> 
         <div class="flier">
-            <p class="flier-link modal-opener" title="Ex-Heir, Static Ghost, Blood Rave. Live at The Coffin Club in Portland, Oregon." data-image="http://bloodrave.tech/wp-content/uploads/2024/07/ex-heir-portland-square.png">
+            <p class="flier-link modal-opener" title="Tassel, Still Ruins, Blood Rave. Live at Thee Stork Club in Oakland, California" data-image="https://bloodrave.tech/wp-content/uploads/2024/07/thee-stork-club-tassel-still-ruins-blood-rave.jpg">
+                09.20.24<br />
+                Thee Stork Club<br />
+                Oakland, CA
+            </p>
+        </div>
+    </li>
+    <li>
+        <div class="info">
+            <p class="lineup modal-opener" data-image="https://bloodrave.tech/wp-content/uploads/2024/07/ex-heir-olympia.png">Ex-Heir, Static Ghost, Blood Rave, Einhänder</p>
+        </div> 
+        <div class="flier">
+            <p class="flier-link modal-opener" title="Ex-Heir, Static Ghost, Blood Rave, Einhander. Live at The Mortuary in Olympia, Washington" data-image="https://bloodrave.tech/wp-content/uploads/2024/07/ex-heir-olympia.png">
+                08.18.24<br />
+                The Mortuary<br />
+                Olympia, WA
+            </p>
+        </div>
+    </li>
+    <li>
+        <div class="info">
+            <p class="lineup modal-opener" data-image="https://bloodrave.tech/wp-content/uploads/2024/07/ex-heir-vancouver-final.png">Ex-Heir, Static Ghost, Blood Rave, Einhänder, Total Chroma</p>
+        </div> 
+        <div class="flier">
+            <p class="flier-link modal-opener" title="Ex-Heir, Static Ghost, Blood Rave, Einhander, Total Chroma. Live at The Cobalt in Vancouver, British Columbia" data-image="https://bloodrave.tech/wp-content/uploads/2024/07/ex-heir-vancouver-final.png">
+                08.17.24<br />
+                The Cobalt<br />
+                Vancouver, BC
+            </p>
+        </div>
+    </li>
+    <li>
+        <div class="info">
+            <p class="lineup modal-opener" data-image="https://bloodrave.tech/wp-content/uploads/2024/07/ex-heir-portland-square.png">Ex-Heir, Static Ghost, Blood Rave</p>
+        </div> 
+        <div class="flier">
+            <p class="flier-link modal-opener" title="Ex-Heir, Static Ghost, Blood Rave. Live at The Coffin Club in Portland, Oregon." data-image="https://bloodrave.tech/wp-content/uploads/2024/07/ex-heir-portland-square.png">
+                08.16.24<br />
                 The Coffin Club<br />
                 Portland, Oregon
             </p>
@@ -13,11 +49,11 @@
     </li>
     <li>
         <div class="info">
-            <p class="date">05.31.24</p>
-            <p class="lineup modal-opener" data-image="http://bloodrave.tech/wp-content/uploads/2024/05/show-flier.jpeg">Houses of Heaven, Ms. Boan, Blood Rave</p>
+            <p class="lineup modal-opener" data-image="https://bloodrave.tech/wp-content/uploads/2024/05/show-flier.jpeg">Houses of Heaven, Ms. Boan, Blood Rave</p>
         </div> 
         <div class="flier">
-            <p class="flier-link modal-opener" title="Houses of Heaven, Ms. Boan, Blood Rave. Live at Thee Stork Club in Oakland, California." data-image="http://bloodrave.tech/wp-content/uploads/2024/05/show-flier.jpeg">
+            <p class="flier-link modal-opener" title="Houses of Heaven, Ms. Boan, Blood Rave. Live at Thee Stork Club in Oakland, California." data-image="https://bloodrave.tech/wp-content/uploads/2024/05/show-flier.jpeg">
+                05.31.24<br />
                 Thee Stork Club<br />
                 Oakland, California
             </p>
