f882e0b29d36d475a6509dc5b8fec732c74c7f67
throne <contact@throne.work>
2024-09-29 12:42:57 -0700

    epk

---
+++ template--epk.php
@@ -0,0 +1,97 @@
+<?php
+/* Template name: Releases */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+get_header(); ?>
+
+
+<div class="epk">
+    <div class="bio">
+        <?php the_content(); ?>
+    </div>
+
+    <div class="split">
+
+        <div class="content">
+
+            <!-- ARTWORK -->
+            <div class="block-grid artwork">
+                <ul>
+                    <li>
+                        <a href="http://bloodrave.tech/wp-content/uploads/2024/09/blurry-ghoul.png" title="Determinate Bias">
+                            <img src="http://bloodrave.tech/wp-content/uploads/2024/09/blurry-ghoul.png" alt="Blood Rave - Determinate Bias - Cover Artwork" />
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- PHOTOS -->
+            <div class="block-grid photos">
+                <ul>
+                    <li>
+                        <a href="http://bloodrave.tech/wp-content/uploads/2024/05/7.png" title="promo-1">
+                            <img src="http://bloodrave.tech/wp-content/uploads/2024/05/7.png" alt="Blood Rave Promo 1"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://bloodrave.tech/wp-content/uploads/2024/09/lawn-edit.jpg" title="promo-2">
+                            <img src="http://bloodrave.tech/wp-content/uploads/2024/09/lawn-edit.jpg" alt="Blood Rave Promo 2"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://bloodrave.tech/wp-content/uploads/2024/09/promo-selfie.png" title="promo-3">
+                            <img src="http://bloodrave.tech/wp-content/uploads/2024/09/promo-selfie.png" alt="Blood Rave Promo 3"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="http://bloodrave.tech/wp-content/uploads/2024/09/woods-selfie.png" title="promo-4">
+                            <img src="http://bloodrave.tech/wp-content/uploads/2024/09/woods-selfie.png" alt="Blood Rave Promo 4"/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="sidebar">
+            <ul class="socials">
+                <li>
+                    <a href="https://bloodrave.bandcamp.com" title="Blood Rave on Bandcamp">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 512 512"><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248s248-111 248-248S393 8 256 8m48.2 326.1h-181L207.9 178h181Z"/></svg>    
+                        <span>Bandcamp</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://open.spotify.com/artist/4beB8JGjQTVp5g6B4h5rls?si=3Cy_JOoeQeejzgn51pjUvA" title="Blood Rave on Spotify">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M17.9 10.9C14.7 9 9.35 8.8 6.3 9.75c-.5.15-1-.15-1.15-.6c-.15-.5.15-1 .6-1.15c3.55-1.05 9.4-.85 13.1 1.35c.45.25.6.85.35 1.3c-.25.35-.85.5-1.3.25m-.1 2.8c-.25.35-.7.5-1.05.25c-2.7-1.65-6.8-2.15-9.95-1.15c-.4.1-.85-.1-.95-.5s.1-.85.5-.95c3.65-1.1 8.15-.55 11.25 1.35c.3.15.45.65.2 1m-1.2 2.75c-.2.3-.55.4-.85.2c-2.35-1.45-5.3-1.75-8.8-.95c-.35.1-.65-.15-.75-.45c-.1-.35.15-.65.45-.75c3.8-.85 7.1-.5 9.7 1.1c.35.15.4.55.25.85M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10a10 10 0 0 0 10-10A10 10 0 0 0 12 2"/></svg>    
+                        <span>Spotify</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://music.apple.com/ca/artist/blood-rave/1716720416" title="Blood Rave on Apple Music">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M23.994 6.124a9.2 9.2 0 0 0-.24-2.19c-.317-1.31-1.062-2.31-2.18-3.043a5 5 0 0 0-1.877-.726a10.5 10.5 0 0 0-1.564-.15c-.04-.003-.083-.01-.124-.013H5.986q-.227.014-.455.026c-.747.043-1.49.123-2.193.4c-1.336.53-2.3 1.452-2.865 2.78c-.192.448-.292.925-.363 1.408a11 11 0 0 0-.1 1.18c0 .032-.007.062-.01.093v12.223l.027.424c.05.815.154 1.624.497 2.373c.65 1.42 1.738 2.353 3.234 2.801c.42.127.856.187 1.293.228c.555.053 1.11.06 1.667.06h11.03a13 13 0 0 0 1.57-.1c.822-.106 1.596-.35 2.295-.81a5.05 5.05 0 0 0 1.88-2.207c.186-.42.293-.87.37-1.324c.113-.675.138-1.358.137-2.04c-.002-3.8 0-7.595-.003-11.393zm-6.423 3.99v5.712c0 .417-.058.827-.244 1.206c-.29.59-.76.962-1.388 1.14q-.524.15-1.07.173c-.95.045-1.773-.6-1.943-1.536a1.88 1.88 0 0 1 1.038-2.022c.323-.16.67-.25 1.018-.324c.378-.082.758-.153 1.134-.24c.274-.063.457-.23.51-.516a1 1 0 0 0 .02-.193q0-2.723-.002-5.443a.7.7 0 0 0-.026-.185c-.04-.15-.15-.243-.304-.234c-.16.01-.318.035-.475.066q-1.14.226-2.28.456l-2.325.47l-1.374.278l-.048.013c-.277.077-.377.203-.39.49q-.002.063 0 .13c-.002 2.602 0 5.204-.003 7.805c0 .42-.047.836-.215 1.227c-.278.64-.77 1.04-1.434 1.233q-.526.152-1.075.172c-.96.036-1.755-.6-1.92-1.544c-.14-.812.23-1.685 1.154-2.075c.357-.15.73-.232 1.108-.31c.287-.06.575-.116.86-.177q.574-.126.6-.714v-.15l.002-8.882c0-.123.013-.25.042-.37c.07-.285.273-.448.546-.518c.255-.066.515-.112.774-.165q1.1-.224 2.2-.444l2.27-.46l2.01-.403c.22-.043.442-.088.663-.106c.31-.025.523.17.554.482q.012.11.012.223q.003 2.866 0 5.732z"/></svg>    
+                        <span>Apple Music</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/jamesrogerx" title="Blood Rave on Instagram">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" color="currentColor"><path d="M2.5 12c0-4.478 0-6.718 1.391-8.109S7.521 2.5 12 2.5c4.478 0 6.718 0 8.109 1.391S21.5 7.521 21.5 12c0 4.478 0 6.718-1.391 8.109S16.479 21.5 12 21.5c-4.478 0-6.718 0-8.109-1.391S2.5 16.479 2.5 12"/><path d="M16.5 12a4.5 4.5 0 1 1-9 0a4.5 4.5 0 0 1 9 0m1.008-5.5h-.01"/></g></svg>    
+                        <span>Instagram</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.x.com/blooodrave" title="Blood Rave on Twitter">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16"><path fill="currentColor" d="M9.294 6.928L14.357 1h-1.2L8.762 6.147L5.25 1H1.2l5.31 7.784L1.2 15h1.2l4.642-5.436L10.751 15h4.05zM7.651 8.852l-.538-.775L2.832 1.91h1.843l3.454 4.977l.538.775l4.491 6.47h-1.843z"/></svg>    
+                        <span>Twitter</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
